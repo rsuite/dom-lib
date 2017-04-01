0a08fbf99604d188af06a83f162a4450398f437e
simonguo <simonguo.2009@gmail.com>
2017-04-01 13:30:17 +0800

    Fixes travis error

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
 
         logLevel: config.LOG_INFO,
 
-        browsers: ['Chrome'],
+        browsers: env.BROWSER ? env.BROWSER.split(',') : ['Chrome'],
 
         preprocessors: {
             'test/html/*.html': 'html2js',
