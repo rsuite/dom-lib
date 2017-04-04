d7a4fb20b8aa644747f416fea6027af6820bd609
simonguo <simonguo.2009@gmail.com>
2017-04-04 10:05:46 +0800

    Update contains

--- src/query/contains.js
+++ src/query/contains.js
@@ -1,22 +1,27 @@
 import canUseDOM from './canUseDOM';
 
 export default (function () {
-    var root = canUseDOM && document.documentElement;
+    const root = canUseDOM && document.documentElement;
 
-    return (root && root.contains) ?
-        (context, node) => {
+    if (root && root.contains) {
+        return (context, node) => {
             return context.contains(node);
-        } : (root && root.compareDocumentPosition) ?
-            (context, node) => {
-                return context === node || !!(context.compareDocumentPosition(node) & 16);
-            } : (context, node) => {
-                if (node) {
-                    do {
-                        if (node === context) {
-                            return true;
-                        }
-                    } while ((node = node.parentNode));
-                }
-                return false;
-            };
+        };
+    } else if (root && root.compareDocumentPosition) {
+        return (context, node) => {
+            return context === node || !!(context.compareDocumentPosition(node) & 16);
+        };
+    } else {
+        return (context, node) => {
+            if (node) {
+                do {
+                    if (node === context) {
+                        return true;
+                    }
+                } while ((node = node.parentNode));
+            }
+            return false;
+        };
+    }
+
 })();
