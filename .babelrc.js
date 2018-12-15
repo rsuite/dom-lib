9dfeb4f2e929a01db0e74ec6ac3c918796ab581d
simonguo <simon.guo@hypers.com>
2018-12-15 14:44:53 +0800

    Upgrade to Bebel 7

---
+++ .babelrc.js
@@ -0,0 +1,31 @@
+module.exports = api => {
+  if (api) {
+    api.cache(() => process.env.NODE_ENV);
+  }
+  return {
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
+    plugins: [
+      '@babel/plugin-transform-proto-to-assign',
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-json-strings',
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-proposal-optional-chaining',
+      [
+        '@babel/plugin-proposal-pipeline-operator',
+        {
+          proposal: 'minimal'
+        }
+      ],
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-do-expressions'
+    ]
+  };
+};
