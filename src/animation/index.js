b409b8dcd345b44ba8a7108463c9727a817d0e22
Godfery <hiyangguo@qq.com>
2020-12-01 19:09:17 +0800

    Add animation events helper

--- src/animation/index.js
+++ src/animation/index.js
@@ -1,3 +1,32 @@
+import { canUseDOM } from '../query';
+
 export cancelAnimationFramePolyfill from './cancelAnimationFramePolyfill';
 export nativeRequestAnimationFrame from './nativeRequestAnimationFrame';
 export requestAnimationFramePolyfill from './requestAnimationFramePolyfill';
+
+const vendorMap = {
+  'animation': 'animationend',
+  'OAnimation': 'oAnimationEnd',
+  'MozAnimation': 'animationend',
+  'WebkitAnimation': 'webkitAnimationEnd'
+};
+
+function getAnimationEvent() {
+  if (!canUseDOM) {
+    return;
+  }
+
+  let tempAnimationEnd;
+  const style = document.createElement('div').style;
+  for (tempAnimationEnd in vendorMap) {
+    if (style[tempAnimationEnd] !== undefined) {
+      return vendorMap[tempAnimationEnd];
+    }
+  }
+}
+
+export const events = ()=>{
+  return {
+   end : getAnimationEvent()
+  }
+};
