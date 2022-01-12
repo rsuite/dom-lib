863e1584ec4be062ad34f3b394c00a37e4d278d6
Doma <leishenghao@126.com>
2022-01-12 19:54:07 +0800

    test(style): re-orgnize test cases for style helpers (#29)

--- test/styleSpec.js
+++ test/styleSpec.js
@@ -5,41 +5,60 @@ describe('Style', () => {
     document.body.innerHTML = window.__html__['test/html/style.html'];
   });
 
-  it('should get style value', () => {
-    const el = document.getElementById('case-1');
-    const color = getStyle(el, 'color');
-    expect(color).to.contain('rgb(255, 0, 0)');
-  });
-
-  it('should get style', () => {
-    const el = document.getElementById('case-1');
-    const style = getStyle(el);
-    expect(style.color).to.contain('rgb(255, 0, 0)');
-    expect(style.marginLeft).to.contain('1px');
-  });
-
-  it('should add style', () => {
-    const el = document.getElementById('case-2');
-    addStyle(el, 'color', '#ffffff');
-    addStyle(el, {
-      background: '#ff0000',
-      'margin-left': '4px'
+  describe('getStyle', () => {
+    it('should return complete style text', () => {
+      const el = document.getElementById('case-1');
+      const style = getStyle(el);
+      expect(style.color).to.contain('rgb(255, 0, 0)');
+      expect(style.marginLeft).to.contain('1px');
     });
 
-    const style = getStyle(el);
-    expect(style.color).to.contain('rgb(255, 255, 255)');
-    expect(style.background).to.contain('rgb(255, 0, 0)');
-    expect(style.marginLeft).to.contain('4px');
+    it('should return style value of specific property', () => {
+      const el = document.getElementById('case-1');
+      const color = getStyle(el, 'color');
+      expect(color).to.contain('rgb(255, 0, 0)');
+    });
   });
 
-  it('should remove style', () => {
-    const el = document.getElementById('case-3');
-    removeStyle(el, 'color');
-    removeStyle(el, ['margin-left', 'margin-right']);
-    const style = getStyle(el);
-    expect(style.color).to.contain('');
-    expect(style.marginLeft).to.contain('');
-    expect(style.marginRight).to.contain('');
-    expect(style.background).to.contain('rgb(255, 0, 0)');
+  describe('addStyle', () => {
+    it('should add a single style property with specific value', () => {
+      const el = document.getElementById('case-2');
+      addStyle(el, 'color', '#ffffff');
+
+      const style = getStyle(el);
+      expect(style.color).to.contain('rgb(255, 255, 255)');
+    });
+
+    it('should add multiple style properties with specific values', () => {
+      const el = document.getElementById('case-2');
+      addStyle(el, {
+        background: '#ff0000',
+        'margin-left': '4px'
+      });
+
+      const style = getStyle(el);
+      expect(style.background).to.contain('rgb(255, 0, 0)');
+      expect(style.marginLeft).to.contain('4px');
+    });
+  });
+
+  describe('removeStyle', () => {
+    it('should remove a single style property', () => {
+      const el = document.getElementById('case-3');
+      removeStyle(el, 'color');
+      const style = getStyle(el);
+      expect(style.color).to.be.empty;
+      expect(style.background).to.contain('rgb(255, 0, 0)');
+    });
+
+    it('should remove multiple style properties', () => {
+      const el = document.getElementById('case-3');
+      removeStyle(el, ['margin-left', 'margin-right']);
+
+      const style = getStyle(el);
+      expect(style.marginLeft).to.be.empty;
+      expect(style.marginRight).to.be.empty;
+      expect(style.background).to.contain('rgb(255, 0, 0)');
+    });
   });
 });
