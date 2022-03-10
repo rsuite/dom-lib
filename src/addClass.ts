3c6985fb584d94a3b05a7ba7e35658c60a3fdd15
Doma <leishenghao@126.com>
2022-03-10 13:30:59 +0800

    docs(addClass): add comment for arguments and return value

--- src/addClass.ts
+++ src/addClass.ts
@@ -1,6 +1,12 @@
 import hasClass from './hasClass';
 
-export default (target: Element, className: string): Element => {
+/**
+ * @param target The element to add class to
+ * @param className The class to be added
+ *
+ * @returns The target element
+ */
+export default function addClass(target: Element, className: string): Element {
   if (className) {
     if (target.classList) {
       target.classList.add(className);
@@ -9,4 +15,4 @@ export default (target: Element, className: string): Element => {
     }
   }
   return target;
-};
+}
